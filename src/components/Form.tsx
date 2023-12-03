a87f5fe2a1b965efe148dec6c1246049832a51e4
musanif <musanifali300@gmail.com>
2023-12-03 09:24:22 +0500

    add alert box and loading button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,12 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import {
+  Alert,
+  AlertIcon,
   Box,
   Button,
   Card,
   CardBody,
+  CardHeader,
   Center,
   FormControl,
   FormLabel,
@@ -12,12 +15,23 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import emailjs from "@emailjs/browser"; // Updated import statement
+import { motion } from "framer-motion";
 
 const Form = () => {
   const Ref = useRef<HTMLFormElement>(null!);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    if (success) {
+      const timeout = setTimeout(() => setSuccess(""), 3000); // Set timeout to match animation
+      return () => clearTimeout(timeout);
+    }
+  }, [success]);
 
   const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
     emailjs
       .sendForm(
         "service_f0x93ob",
@@ -27,46 +41,91 @@ const Form = () => {
       )
       .then(
         (result) => {
+          setSuccess("Sent Successfully!");
+          setIsLoading(false);
           console.log(result.text);
         },
-        (error) => {
-          console.log(error.text);
+        (error: any) => {
+          console.log(error);
+          setError(error);
+          setIsLoading(false);
         }
       );
   };
 
   return (
-    <Box>
-      <Center>
-        <Heading mt={5} as="h1" size="xl" mb={6}>
-          Say Hello
-        </Heading>
-      </Center>
-      <Card margin="10px 10%">
-        <CardBody>
-          <form ref={Ref} onSubmit={sendEmail}>
-            <FormControl id="name" isRequired>
-              <FormLabel>Name</FormLabel>
-              <Input type="text" name="user_name" />
-            </FormControl>
+    <>
+      <Box>
+        <Center>
+          <Heading mt={20} as="h1" size="xl" mb={6}>
+            Say Hello
+          </Heading>
+        </Center>
+        <Card margin="10px 10%">
+          <CardHeader>
+            {error && (
+              <motion.div
+                initial={{ x: -500, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -500, opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <Alert borderRadius={7} p={5} status="error">
+                  <AlertIcon />
+                  {error}
+                </Alert>
+              </motion.div>
+            )}
+            {success && (
+              <motion.div
+                initial={{ x: -500, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -500, opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <Alert borderRadius={7} p={5} status="success">
+                  <AlertIcon />
+                  {success}
+                </Alert>
+              </motion.div>
+            )}
+          </CardHeader>
+          <CardBody>
+            <form ref={Ref} onSubmit={sendEmail}>
+              <FormControl id="name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input type="text" name="user_name" />
+              </FormControl>
 
-            <FormControl id="email" isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" name="user_email" />
-            </FormControl>
+              <FormControl id="email" isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" name="user_email" />
+              </FormControl>
 
-            <FormControl id="message" isRequired>
-              <FormLabel>Message</FormLabel>
-              <Textarea name="message" />
-            </FormControl>
-
-            <Button type="submit" mt={4}>
-              Send
-            </Button>
-          </form>
-        </CardBody>
-      </Card>
-    </Box>
+              <FormControl id="message" isRequired>
+                <FormLabel>Message</FormLabel>
+                <Textarea name="message" />
+              </FormControl>
+              {isLoading ? (
+                <Button
+                  isLoading
+                  loadingText="Sending"
+                  variant="outline"
+                  type="submit"
+                  mt={4}
+                >
+                  Send
+                </Button>
+              ) : (
+                <Button type="submit" mt={4}>
+                  Send
+                </Button>
+              )}
+            </form>
+          </CardBody>
+        </Card>
+      </Box>
+    </>
   );
 };
 
